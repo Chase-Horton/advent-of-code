9e891ea2f06d5bad8ef45ecbee068b681b2ad90e
chase <chasejhorton@gmail.com>
2023-09-17 10:49:07 -0500

    solved day 4 part 1

---
+++ solutions/day4.py
@@ -0,0 +1,28 @@
+def loadAssignments():
+    with open('../data/day4.txt') as f:
+        data = f.read()
+    assignments = []
+    for line in data.strip().split('\n'):
+        as1, as2 = line.split(',')
+        assignments.append([as1.split('-'), as2.split('-')])
+    return assignments
+
+def doesListContain(outer, inner):
+    if int(inner[0]) < int(outer[0]):
+        return False
+    if int(inner[1]) > int(outer[1]):
+        return False
+    return True
+
+#solution 1
+assignments = loadAssignments()
+pairs = 0
+for assignment in assignments:
+    pairIsMatch = doesListContain(*assignment)
+    if not pairIsMatch:
+        assignment.reverse()
+        pairIsMatch = doesListContain(*assignment)
+    if pairIsMatch:
+        pairs += 1
+
+print('Solution 1:', pairs)
