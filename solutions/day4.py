6f20f309b95a56c408010f9ff1c08d4cb838b4a6
chase <chasejhorton@gmail.com>
2023-09-17 16:59:42 -0500

    solution 2

--- solutions/day4.py
+++ solutions/day4.py
@@ -26,3 +26,15 @@ for assignment in assignments:
         pairs += 1
 
 print('Solution 1:', pairs)
+# solution 2
+def doesListOverlapAtAll(outer, inner):
+    outer = set(range(int(outer[0]), int(outer[1]) + 1))
+    inner = set(range(int(inner[0]), int(inner[1]) + 1))
+    overlaps = len( outer & inner )
+    return overlaps > 0
+pairs = 0
+for assignment in assignments:
+    pairIsMatch = doesListOverlapAtAll(*assignment)
+    if pairIsMatch:
+        pairs += 1
+print('Solution 2:', pairs)
