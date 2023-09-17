5d6572f1fb3851e5704bd38fa82a647324b4a35e
chase <chasejhorton@gmail.com>
2023-09-17 09:10:45 -0500

    day 2 solution 2

--- solutions/day2.py
+++ solutions/day2.py
@@ -1,6 +1,5 @@
 keys = {"A" : 1, "B" : 2, "C" : 3, "X" : 1, "Y" : 2, "Z" : 3}
 def compare(a, b):
-    a, b = keys[a], keys[b]
     points = b
     # draw
     if a == b:
@@ -13,7 +12,26 @@ def compare(a, b):
 with open('../data/day2.txt') as f:
     data = f.read()
 points = 0
+# solution 1
 for line in data.split('\n')[:-1]:
     a, b = line.split(' ')
+    points += compare(keys[a], keys[b])
+print('Solution 1:', points)
+#solution 2
+points = 0
+for line in data.split('\n')[:-1]:
+    a, b = line.split(' ')
+    a, b = keys[a], keys[b]
+    # need to lose
+    if b == 1:
+        b = a - 1
+        if b < 1:
+            b = 3
+    # need to draw
+    elif b == 2:
+        b = a
+    # need to win
+    elif b == 3:
+        b = (a % 3) + 1
     points += compare(a, b)
-print('Solution:', points)
+print('Solution 2:', points)
