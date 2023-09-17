7b3577e177b896945194e6b1755f8806628bc70e
chase <chasejhorton@gmail.com>
2023-09-17 18:33:25 -0500

    day 5 solutions

--- solutions/day5.py
+++ solutions/day5.py
@@ -9,7 +9,6 @@ class Stacker:
         self.getKeysAndStacks(layer)
         #fill stacklists with obj from column
         self.populateStacks(layers)
-
     def getKeysAndStacks(self, baseLayer):
         for i, item in enumerate(baseLayer):
             if item != ' ':
@@ -23,18 +22,22 @@ class Stacker:
                   self.stacks[key].append(indexVal)
     def move(self, string):
         amt, fromId, toId = re.findall(r'\b\d+\b', string)
-        #print(self.stacks)
-        #print(f'moving {amt} from {fromId} to {toId}')
         for _ in range(int(amt)):
             self.stacks[toId].append(self.stacks[fromId].pop())
-        #print(self.stacks)
+    def moveSlice(self, string):
+        amt, fromId, toId = re.findall(r'\b\d+\b', string)
+        if amt == 1:
+            self.stacks[toId].append(self.stacks[fromId].pop())
+            return
+        self.stacks[toId] += self.stacks[fromId][-int(amt):]
+        self.stacks[fromId] = self.stacks[fromId][:-int(amt)]
     def printTops(self):
         finalStr = ''
         for key in self.stacks:
             top = self.stacks[key].pop()
-            print(f'{key}:{top}')
+            print(f'{key}:{self.stacks[key]}')
             finalStr += top
-        print(finalStr)
+        print('Stack Out:', finalStr)
 with open('../data/day5.txt') as f:
     data = f.read()
 # solution 1
@@ -45,4 +48,12 @@ for line in data[1].split('\n'):
     if line == '':
         break
     S.move(line)
+S.printTops()
+
+# solution 2
+S = Stacker(stacks)
+for line in data[1].split('\n'):
+    if line == '':
+        break
+    S.moveSlice(line)
 S.printTops()
