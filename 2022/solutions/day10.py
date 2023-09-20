9807ab1e73b02ee26ebeed1c6bd915efac7d83a8
Chase <chasehortonj@gmail.com>
2023-09-20 03:20:30 -0500

    day 10 with pretty printing and verbose debug

--- 2022/solutions/day10.py
+++ 2022/solutions/day10.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env python3
+import os
+from time import sleep
 with open('../data/day10.txt') as f:
     data = f.read().strip()
 class VM:
-    def __init__(self, words):
+    def __init__(self, words, verbose = False, pretty = False):
         self.clock = 0
         self.CRT = ''
         self.words = words
         self.reg = 1
         self.s = 0
+        self.verbose = verbose
+        self.pretty = pretty
     def getCRT(self):
         return self.clock - 1
     def startCycle(self):
         self.clock += 1
-        print(f'start cycle {self.clock}')
+        if self.verbose:
+            print(f'start cycle {self.clock}')
     def endCycle(self, word):
         if word != 'noop' and word != 'addx':
             self.reg += int(word)
             endStr = f'End of cycle: added {int(word)}, Register X is now {self.reg}\n'
         else:
             endStr = f'End of cycle: {word}\n'
-        print(endStr)
+        if self.verbose:
+            print(endStr)
     def cycle(self):
         # print(f'Clock:{self.clock}, reg:{self.reg}')
-        print(f'In Cycle: clock/{self.clock}, register/{self.reg}')
+        if self.verbose:
+            print(f'In Cycle: clock/{self.clock}, register/{self.reg}')
         if (self.reg -1 <= (self.clock % 40) - 1 <= self.reg + 1):
             self.CRT += '#'
         else:
             self.CRT += '.'
         if (self.clock % 40) == 20:
-            print(self.clock * self.reg)
             self.s += self.clock * self.reg
         if(self.clock % 40) == 0:
             self.CRT += '\n'
-        print(self.CRT)
+        if self.pretty:
+            os.system('clear')
+            print(self.CRT)
+            sleep(0.17)
         #self.outPut
     def run(self):
         for word in self.words:
@@ -45,6 +54,6 @@ flatWords = []
 for line in data.split('\n'):
     flatWords += line.split(' ')
 #swap addxs for noop as we can just treat them that way and since addition happens 1 cycle after treat numbers as addition instructions
-V = VM(flatWords)
+V = VM(flatWords, pretty=True)
 V.run()
 print(V.CRT)
